56a303ace4672dae51e32daa5911cb12c1c81a6c
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-01-06 19:28:41 +0100

    HomeView layout finished; styles moved succesfully to separate file

--- src/sections/endpoints/EndpointsList.js
+++ src/sections/endpoints/EndpointsList.js
@@ -17,52 +17,52 @@ class EndpointsList extends Component {
     render() {
         return (
 
-            <View style={styles.mainContainer}>
+            <View style={EndpointsStyle.mainContainer}>
 
 
-                <View style={styles.smallContainer}>
-                    <TouchableOpacity style={styles.sectionContainer} onPress={() => Actions.CharactersList()}>
-                        <Image style={styles.image}
+                <View style={EndpointsStyle.smallContainer}>
+                    <TouchableOpacity style={EndpointsStyle.sectionContainer} onPress={() => Actions.CharactersList()}>
+                        <Image style={EndpointsStyle.image}
                             source={require('reactMarvel/src/images/characters.jpg')}
                             resizeMode={'cover'} />
-                        <View style={styles.textContainer}>
-                            <Text style={styles.textStyle}>{'Characters'}</Text>
+                        <View style={EndpointsStyle.textContainer}>
+                            <Text style={EndpointsStyle.textStyle}>{'Characters'}</Text>
                         </View>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.sectionContainer} onPress={() => Actions.CharactersMyList()}>
-                        <Image style={styles.image}
+                    <TouchableOpacity style={EndpointsStyle.sectionContainer} onPress={() => Actions.CharactersMyList()}>
+                        <Image style={EndpointsStyle.image}
                             source={require('reactMarvel/src/images/mycharacters.jpg')}
                             resizeMode={'cover'} />
-                        <View style={styles.textContainer}>
-                            <Text style={styles.textStyle}>{'My Characters'}</Text>
+                        <View style={EndpointsStyle.textContainer}>
+                            <Text style={EndpointsStyle.textStyle}>{'My Characters'}</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
 
 
-                <View style={styles.smallContainer}>
-                    <TouchableOpacity style={styles.sectionContainer}>
-                        <Image style={styles.image}
+                <View style={EndpointsStyle.smallContainer}>
+                    <TouchableOpacity style={EndpointsStyle.sectionContainer}>
+                        <Image style={EndpointsStyle.image}
                             source={require('reactMarvel/src/images/creators.png')}
                             resizeMode={'cover'} />
-                        <View style={styles.textContainer}>
-                            <Text style={styles.textStyle}>{'Creators'}</Text>
+                        <View style={EndpointsStyle.textContainer}>
+                            <Text style={EndpointsStyle.textStyle}>{'Creators'}</Text>
                         </View>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.sectionContainer}>
-                        <Image style={styles.image}
+                    <TouchableOpacity style={EndpointsStyle.sectionContainer}>
+                        <Image style={EndpointsStyle.image}
                             source={require('reactMarvel/src/images/comics.jpg')}
                             resizeMode={'cover'} />
-                        <View style={styles.textContainer}>
-                            <Text style={styles.textStyle}>{'Comics'}</Text>
+                        <View style={EndpointsStyle.textContainer}>
+                            <Text style={EndpointsStyle.textStyle}>{'Comics'}</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
 
 
-                <View style={styles.spinner}>
+                <View style={EndpointsStyle.spinner}>
                     <Spinner  size={100} type={'Bounce'} color={'white'} isVisible={this.props.isFetching} />
                 </View>
 
@@ -93,63 +93,4 @@ const mapDispatchToProps = (dispatch, props) => {
 
 export default connect(mapStateToProps, mapDispatchToProps)(EndpointsList)
 
-const styles = StyleSheet.create({
-
-    textStyle: {
-        color: 'white',
-        textAlign: 'center',
-        fontSize: 20 ,
-        fontWeight: '500', 
-        padding: 10,
-        paddingHorizontal: 5
-    },
-
-    textContainer: {
-        position: 'absolute',
-        padding: 1,
-        right: 0,
-        left: 0,
-        top: (Dimensions.get('window').height / 2 - 50) / 2-30,
-        backgroundColor: 'rgba(128,128,128,0.8)',
-    },
-
-    image: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        width: Dimensions.get('window').width / 2 - 20,
-        height: Dimensions.get('window').height / 2 - 50,
-    },
-
-    sectionContainer: {
-        margin: 10,
-        width: Dimensions.get('window').width / 2 - 20,
-        height: Dimensions.get('window').height / 2 - 20,
-    },
-
-    mainContainer: {
-        flex: 1,
-        backgroundColor: 'rgb(222,53,46)',
-    },
-
-    smallContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        paddingBottom: 20,
-        marginBottom: 10,
-    },
-
-    spinner: {
-        marginBottom: 50,
-        alignItems: 'center',
-        position: 'absolute',
-        top: Dimensions.get('window').height / 2 - 80,
-        //top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
-    }
-})
 
