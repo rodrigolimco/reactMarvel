35c39f803b6e7c76a59183f3c16235c34ff0bc03
Rodrigo Limpias <rodrigolimpiascossio@hotmail.com>
2018-01-06 13:57:26 +0100

    Spinner installed

--- android/app/src/main/java/com/reactmarvel/MainApplication.java
+++ android/app/src/main/java/com/reactmarvel/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactmarvel;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNSpinkitPackage(),
+          new ImagePickerPackage()
       );
     }
 
